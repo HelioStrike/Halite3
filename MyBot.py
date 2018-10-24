2186132c7e07032c25756d8680611cd38448a41a
HelioStrike <rageofepicfury@gmail.com>
2018-10-24 22:29:14 +0530

    added slightly less random navigation

--- MyBot.py
+++ MyBot.py
@@ -8,7 +8,7 @@ import hlt
 from hlt import constants
 
 # This library contains direction metadata to better interface with the game.
-from hlt.positionals import Direction
+from hlt.positionals import Direction, Position
 
 # This library allows you to generate random numbers.
 import random
@@ -35,7 +35,20 @@ logging.info("Successfully created bot! My Player ID is {}.".format(game.my_id))
 
 directions = [Direction.North, Direction.South, Direction.East, Direction.West, Direction.Still]
 
-ship_status = {}
+#stores harvestable(is that a word?) points
+meat_points = []
+
+game_map = game.game_map
+
+#storing meat points
+for x in range(game_map.height):
+    for y in range(game_map.height):
+        pos = Position(x, y)
+        if game_map[pos].halite_amount > constants.MAX_HALITE/5:
+            meat_points.append(pos)
+
+#maps ship id to their designated destination
+ship_dests = {} 
 
 """ <<<Game Loop>>> """
 while True:
@@ -46,49 +59,62 @@ while True:
     me = game.me
     game_map = game.game_map
 
-
     # A command queue holds all the commands you will run this turn. You build this list up and submit it at the
     #   end of the turn.
     command_queue = []
+
+    #stores current and next positions of ships, so that other ships can avoid these stops(even turtles need personal space!)
     ship_positions = []
+
+    #storing initial positions of ships
+    for ship in me.get_ships():
+        ship_positions.append(ship.position)
+
 
     for ship in me.get_ships():
         # For each of your ships, move randomly if the ship is on a low halite location or the ship is full.
         #   Else, collect halite.
-        if ship.id not in ship_status.keys() or ship.position == me.shipyard.position:
-            ship_status[ship.id] = "exploring" 
 
+        #if the ship id doesn't exist in ship_dests or the ship is at the shipyard, give his life a meaning
+        if ship.id not in ship_dests.keys() or ship.position == me.shipyard.position:
+            ship_dests[ship.id] = meat_points[random.randint(0, len(meat_points)-1)]
+
+        ship_positions.append(ship.position)
+
+        #if ship is full, draw him to the shipyard
         if ship.is_full:
-            ship_status[ship.id] = "returning"
+            ship_dests[ship.id] = me.shipyard.position
 
-        position_options = ship.position.get_surrounding_cardinals() + [ship.position]
-
-        halite_amounts = []
-
-        for i, direction in enumerate(directions):
-            if game_map[position_options[i]].is_occupied:
-                ship_positions.append(position_options[i])
-            else:
-                halite_amounts.append(game_map[position_options[i]].halite_amount)
-        
-        halite_amounts = np.array(halite_amounts)
-        halite_amounts = halite_amounts/np.sum(halite_amounts)
-        halite_amounts = [np.asscalar(item) for item in halite_amounts]
-        logging.info(halite_amounts)
-
-        if ship_status[ship.id] == "exploring":
-            if game_map[ship.position].halite_amount < constants.MAX_HALITE / 10 or ship.is_full:
-                for p in range(4):
-                    next_choice = choice(5, 1, halite_amounts)[0]
-                    next_position = position_options[next_choice]
-                    if next_position not in ship_positions:
-                        ship_positions.append(next_position)
-                        command_queue.append(ship.move(directions[next_choice]))
-                        break    
-            else:
-                command_queue.append(ship.stay_still())
-        elif ship_status[ship.id] == "returning":
-            command_queue.append(ship.move(game_map.naive_navigate(ship, me.shipyard.position)))
+        #if its a returning ship
+        if ship_dests[ship.id] == me.shipyard.position:
+            #the next 10 lines of code make sure that the ship doesn't collide with any other step, and updates ship_positions
+            next_move = game_map.naive_navigate(ship, me.shipyard.position)
+            if next_move == Direction.Still or ship.position.addDirection(next_move) in ship_positions:
+                for p in range(5):
+                    move = directions[random.randint(0, 3)]
+                    if(not game_map[ship.position.addDirection(move)].is_occupied and ship.position.addDirection(move) not in ship_positions):
+                        next_move = move
+                        break
+            ship_positions.append(ship.position.addDirection(next_move))
+            command_queue.append(ship.move(next_move))
+        #else if the ship's cell if almost clear
+        elif game_map[ship.position].halite_amount < constants.MAX_HALITE/10:
+            #if ship is at its destination((possibly former) meat point), remove it from meat points as it is exhausted
+            if ship.position == ship_dests[ship.id]:
+                if ship.position in meat_points:
+                    meat_points.remove(ship.position)
+                ship_dests[ship.id] = meat_points[random.randint(0, len(meat_points)-1)]
+                
+            #the next 10-ish lines of code make sure that the ship doesn't collide with any other step, and updates ship_positions
+            next_move = game_map.naive_navigate(ship, ship_dests[ship.id])
+            if next_move == Direction.Still or ship.position.addDirection(next_move) in ship_positions:
+                for p in range(5):
+                    move = directions[random.randint(0, 3)]
+                    if(not game_map[ship.position.addDirection(move)].is_occupied and ship.position.addDirection(move) not in ship_positions):
+                        next_move = move
+                        break
+            ship_positions.append(ship.position.addDirection(next_move))
+            command_queue.append(ship.move(next_move))
 
     # If the game is in the first 200 turns and you have enough halite, spawn a ship.
     # Don't spawn a ship if you currently have a ship at port, though - the ships will collide.
